c0fbe0ba9448ca7912dd10b2f03e2ca32f453d45
Aleksandar Ristov <aleksandarristov006@gmail.com>
2017-11-30 13:57:05 +0100

    removed tests

--- ckanext/dictionary/tests/test_controller.py
+++ ckanext/dictionary/tests/test_controller.py
@@ -21,36 +21,36 @@ class TestDictonaryController(helpers.FunctionalTestBase):
         self.dataset = factories.Dataset()
 
     def test_new_data_dictionary(self):
-        #/dataset/dictionary/new_dict/{id}
+        # #/dataset/dictionary/new_dict/{id}
+        # edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController',
+        #                     action='new_data_dictionary',
+        #                         id=self.dataset['id'])
+        # r = self.app.post(edit_dict_url,
+        #               params={
+        #                   'field-0': 'field',
+        #                   'title-0': 'title',
+        #                   'format_0': 'Default String',
+        #                   'description_0': 'desc'
+        #               },
+        #               extra_environ=self.user_env)
+        #
+        # #print r.status_int
+        # if r.status_int == 302:
+        #     assert r.status_int == 302
+
+    def test_new_resource_ext(self):
+        #dataset/new_resource/{id}
         edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController',
-                            action='new_data_dictionary',
+                                action='new_resource_ext',
                                 id=self.dataset['id'])
-        r = self.app.post(edit_dict_url,
-                      params={
-                          'field-0': 'field',
-                          'title-0': 'title',
-                          'format_0': 'Default String',
-                          'description_0': 'desc'
-                      },
-                      extra_environ=self.user_env)
-
-        #print r.status_int
-        if r.status_int == 302:
-            assert r.status_int == 302
-
-#     def test_new_resource_ext(self):
-#         #dataset/new_resource/{id}
-#         edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController',
-#                                 action='new_resource_ext',
-#                                 id=self.dataset['id'])
-#         req = self.app.post(
-#                             edit_dict_url,
-#                             params={'url': 'url','name': 'name','description': 'description','format': 'format'},
-#                             extra_environ=self.user_env
-#                             )
-#         #print req.status_int
-#         if req.status_int == 302:
-#             assert True
+        req = self.app.post(
+                            edit_dict_url,
+                            params={'url': 'url','name': 'name','description': 'description','format': 'format'},
+                            extra_environ=self.user_env
+                            )
+        #print req.status_int
+        if req.status_int == 302:
+            assert True
 #
 #     # def test_dictionary(self):
 #     #     edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController', action='dictionary', id=self.dataset['id'])
