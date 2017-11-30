8bf0c857a84c4cd226f6d818d4224ca8d8c18685
Aleksandar Ristov <aleksandarristov006@gmail.com>
2017-11-30 14:05:20 +0100

    remove tests

--- ckanext/dictionary/tests/test_controller.py
+++ ckanext/dictionary/tests/test_controller.py
@@ -36,21 +36,21 @@ class TestDictonaryController(helpers.FunctionalTestBase):
         #
         # #print r.status_int
         # if r.status_int == 302:
-        #     assert r.status_int == 302
+        #     assert r.status_int == 302/
 
-    def test_new_resource_ext(self):
-        #dataset/new_resource/{id}
-        edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController',
-                                action='new_resource_ext',
-                                id=self.dataset['id'])
-        req = self.app.post(
-                            edit_dict_url,
-                            params={'url': 'url','name': 'name','description': 'description','format': 'format'},
-                            extra_environ=self.user_env
-                            )
-        #print req.status_int
-        if req.status_int == 302:
-            assert True
+    # def test_new_resource_ext(self):
+    #     #dataset/new_resource/{id}
+    #     edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController',
+    #                             action='new_resource_ext',
+    #                             id=self.dataset['id'])
+    #     req = self.app.post(
+    #                         edit_dict_url,
+    #                         params={'url': 'url','name': 'name','description': 'description','format': 'format'},
+    #                         extra_environ=self.user_env
+    #                         )
+    #     #print req.status_int
+    #     if req.status_int == 302:
+    #         assert True
 #
 #     # def test_dictionary(self):
 #     #     edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController', action='dictionary', id=self.dataset['id'])
