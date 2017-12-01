ff9b3b973f6d04708088f820b627b61349f23075
Aleksandar Ristov <aleksandarristov006@gmail.com>
2017-12-01 11:48:33 +0100

    latest commit :D

--- ckanext/dictionary/tests/test_controller.py
+++ ckanext/dictionary/tests/test_controller.py
@@ -38,61 +38,61 @@ class TestDictonaryController(helpers.FunctionalTestBase):
         if r.status_int == 302:
             assert r.status_int == 302
 
-    # def test_new_resource_ext(self):
-    #     #dataset/new_resource/{id}
-    #     edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController',
-    #                             action='new_resource_ext',
-    #                             id=self.dataset['id'])
-    #     req = self.app.post(
-    #                         edit_dict_url,
-    #                         params={'url': 'url','name': 'name','description': 'description','format': 'format'},
-    #                         extra_environ=self.user_env
-    #                         )
-    #     #print req.status_int
-    #     if req.status_int == 302:
-    #         assert True
-#
-#     # def test_dictionary(self):
-#     #     edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController', action='dictionary', id=self.dataset['id'])
-#     #     req = self.app.post(edit_dict_url, params=)
-#
-#     def test_edit_dictionary(self):
-#         #/dataset/dictionary/edit/{id}
-#         edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController',
-#                                 action='edit_dictionary',
-#                                 id=self.dataset['id'])
-#
-#         for i in range(10):
-#             r = self.app.post(edit_dict_url,
-#                               params={
-#                                   'field': 'field_'+str(i),
-#                                   'title': 'title_'+str(i),
-#                                   'format': 'format_'+str(i),
-#                                   'description': 'description_'+str(i)
-#                               },
-#                               extra_environ=self.user_env
-#                               )
-#             if r.status_int == 302:
-#                 assert True
-#
-#     def test_finaldict(self):
-#         #/dataset/dictionary/add/{id}
-#         edit_dict_url = url_for(
-#             controller='ckanext.dictionary.controller:DDController',
-#             action='finaldict',
-#             id=self.dataset['id']
-#         )
-#         for i in range(3):
-#             r = self.app.post(
-#                 edit_dict_url,
-#                 params={'field' : 'field_'+str(i),
-#                         'title': 'title_'+str(i),
-#                         'format' : 'format_'+str(i),
-#                         'description' : 'description_'+str(i)
-#                         },
-#                 extra_environ=self.user_env
-#                 )
-#
-#             if r.status_int == 302:
-#                 assert True
-#
+    def test_new_resource_ext(self):
+        #dataset/new_resource/{id}
+        edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController',
+                                action='new_resource_ext',
+                                id=self.dataset['id'])
+        req = self.app.post(
+                            edit_dict_url,
+                            params={'url': 'url','name': 'name','description': 'description','format': 'format'},
+                            extra_environ=self.user_env
+                            )
+        #print req.status_int
+        if req.status_int == 302:
+            assert True
+
+    # def test_dictionary(self):
+    #     edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController', action='dictionary', id=self.dataset['id'])
+    #     req = self.app.post(edit_dict_url, params=)
+
+    def test_edit_dictionary(self):
+        #/dataset/dictionary/edit/{id}
+        edit_dict_url = url_for(controller='ckanext.dictionary.controller:DDController',
+                                action='edit_dictionary',
+                                id=self.dataset['id'])
+
+        for i in range(10):
+            r = self.app.post(edit_dict_url,
+                              params={
+                                  'field': 'field_'+str(i),
+                                  'title': 'title_'+str(i),
+                                  'format': 'format_'+str(i),
+                                  'description': 'description_'+str(i)
+                              },
+                              extra_environ=self.user_env
+                              )
+            if r.status_int == 302:
+                assert True
+
+    def test_finaldict(self):
+        #/dataset/dictionary/add/{id}
+        edit_dict_url = url_for(
+            controller='ckanext.dictionary.controller:DDController',
+            action='finaldict',
+            id=self.dataset['id']
+        )
+        for i in range(3):
+            r = self.app.post(
+                edit_dict_url,
+                params={'field' : 'field_'+str(i),
+                        'title': 'title_'+str(i),
+                        'format' : 'format_'+str(i),
+                        'description' : 'description_'+str(i)
+                        },
+                extra_environ=self.user_env
+                )
+
+            if r.status_int == 302:
+                assert True
+
